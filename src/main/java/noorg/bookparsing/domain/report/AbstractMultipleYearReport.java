2dd93b902da176f68a3a7c08bc12afca99375ef7
Robert J. Zak <robertjzak@gmail.com>
2016-12-08 16:31:52 +0000

    Adding Javadoc for AbstractMultipleYearReport

--- src/main/java/noorg/bookparsing/domain/report/AbstractMultipleYearReport.java
+++ src/main/java/noorg/bookparsing/domain/report/AbstractMultipleYearReport.java
@@ -9,6 +9,26 @@ import java.util.TreeSet;
 import noorg.bookparsing.domain.Book;
 import noorg.bookparsing.report.format.BookFormatter;
 
+/**
+ * <p>Copyright 2016 Robert J. Zak
+ * 
+ * <p>Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * <p>    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * <p>Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * <p>Shared code for all Multiple Year Reports
+ * 
+ * @author Robert J. Zak
+ *
+ */
 public abstract class AbstractMultipleYearReport extends AbstractReport{
 	public static final int UNKNOWN_YEAR = Integer.MIN_VALUE;
 
